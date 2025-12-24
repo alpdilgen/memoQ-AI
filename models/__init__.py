a5ea1d3c7a93e745e48044686bd965e02f9797b3
alpdilgen <info@anova.bg>
2025-12-24 10:46:38 +0300

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,1 +1,18 @@
+"""Models package"""
+from .entities import (
+    TranslationSegment,
+    TMMatch,
+    TermMatch,
+    BatchResult,
+    TranslationMetadata,
+    SegmentAnalysis
+)
 
+__all__ = [
+    'TranslationSegment',
+    'TMMatch',
+    'TermMatch',
+    'BatchResult',
+    'TranslationMetadata',
+    'SegmentAnalysis'
+]
